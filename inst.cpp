dfd7032670e3de6282c021ff09628ea401ea37d7
Rostislav Gogolauri <go.go.gg.rostislav@gmail.com>
2017-09-06 16:49:34 +0300

    Add files via upload

--- inst.cpp
+++ inst.cpp
@@ -6,22 +6,13 @@
 #include <conio.h>                 //include the debug library
 using namespace std;
         // Let`s move out!
-class Class1 {
-public:
-	Class1(int val) { this->value = val; }
-	Class1(Class1 const &source) { value = source.value + 100; }
-	int value;
-};
-class Class2 {
-public:
-	Class2(int val) { this->value = val; }
-	int value;
-};
+void fun(void) {
+	/*auto */ int var = 99;
+	cout << "var = " << ++var << endl;
+}
 int main(void) {
-	Class1 object11(100), object12 = object11;
-	Class2 object21(200), object22 = object21;
-	cout << object12.value << endl;
-	cout << object22.value << endl;
+	for (int i = 0; i < 5; i++)
+		fun();
 	getch();    //debug point
 	return 0;
 }
