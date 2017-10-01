44ec5a465a82e998ed1608ed7be8dbb7f1093068
Rostislav Gogolauri <go.go.gg.rostislav@gmail.com>
2017-10-01 08:44:48 +0300

    Update MathProf.cpp

--- MathProf.cpp
+++ MathProf.cpp
@@ -1,9 +1,9 @@
-﻿// Written by Rostisval Gogolauri /2017/
-// Telegram @Map_Inc
+﻿// Written by Rostisval Gogolauri /2017//
+// Telegram @Map_Inc//
 
 #include "stdafx.h"
 #include <iostream>
-#include <conio.h> // debug library
+#include <conio.h> // debug library//
 #include <string>
 #include <math.h>
 #define PI 3.14159265
@@ -13,20 +13,20 @@ int main()
 {
 
 	int ink = 1;
-	float x0, x, y0, y, z0, z, c, k,x1,y1,z1;			    //coordinates * координаты
-	system("Color 2");	// f0 - white and black		// change text and background color of console * цвета для фона, текста
-	while (ink == 1) {                          // loop for inputing without exiting command line * Ввод след. без закрытия консоли 
+	float x0, x, y0, y, z0, z, c, k,x1,y1,z1;			    //coordinates * координаты//
+	system("Color 2");	// f0 - white and black		 change text and background color of console * цвета для фона, текста //
+	while (ink == 1) {                          // loop for inputing without exiting command line * Ввод след. без закрытия консоли //
 		cout << "Choise one statement which you need to solve (1-7)" << endl;
 		cout << " " << endl;
 		cout << "1: General equation of line Ax+By+C=0" << endl;												   //1: Загальне рівняня прямої 
-		cout << "2: Equation of line using line segments x/a+y/b=1 " << endl;								       //2: Векторне рівняня прямої  у відрізках
-		cout << "3: Equation of line with angle coefficient y=kx+b" << endl;							           //3:  Рівняня прямої з кутовим коефіціентом k
-		cout << "4: Equation of line going through 2 points (x-x1)/(x2-x1)=(y-y1)/(y2-y1)" << endl;			       //4: Відстань від точки до прямої
-		cout << "5: Normal equation of line x*cos(a)+y*sin(a)-p=0" << endl;										   //5: Нормальне рівнянян прямої
-		cout << "6: Equation of line with parameters x=x1+a*(lambda)" << "  y=y1+b*(lambda)" << endl;			   //6: Параметричне рівняня прямої
-		cout << "7: Equation of line in the open space" << endl;                                                   //7: Рівняня прямої в просторі, що проходить через 2 точки
-		cout << "8: Angle between plane and line (alpha)" << endl;                                                 //8: Кут мiж площин. i прямою
-		cout << "9: Scalar equation of the plane ax+by+cz=d" << endl;											   //9: Скалярне рiвняня площини 
+		cout << "2: Equation of line using line segments x/a+y/b=1 " << endl;								       //2: Векторне рівняня прямої  у відрізках//
+		cout << "3: Equation of line with angle coefficient y=kx+b" << endl;							           //3:  Рівняня прямої з кутовим коефіціентом k//
+		cout << "4: Equation of line going through 2 points (x-x1)/(x2-x1)=(y-y1)/(y2-y1)" << endl;			       //4: Відстань від точки до прямої//
+		cout << "5: Normal equation of line x*cos(a)+y*sin(a)-p=0" << endl;										   //5: Нормальне рівнянян прямої//
+		cout << "6: Equation of line with parameters x=x1+a*(lambda)" << "  y=y1+b*(lambda)" << endl;			   //6: Параметричне рівняня прямої//
+		cout << "7: Equation of line in the open space" << endl;                                                   //7: Рівняня прямої в просторі, що проходить через 2 точки//
+		cout << "8: Angle between plane and line (alpha)" << endl;                                                 //8: Кут мiж площин. i прямою//
+		cout << "9: Scalar equation of the plane ax+by+cz=d" << endl;											   //9: Скалярне рiвняня площини //
 	
 		int caser;
 		cin >> caser;
@@ -60,8 +60,8 @@ int main()
 			}
 			break;
 		case 2:cout << "Input please 2 line segments a,b:";
-			cin >> x >> y;    //x=a, y=b
-			system("cls"); // clears the screen * Оч. скрин
+			cin >> x >> y;    //x=a, y=b//
+			system("cls"); // clears the screen * Оч. скрин//
 			if (x == 0 || y == 0) {
 				cout << "division by zero!!!!!!!!!!!";
 			}
@@ -80,8 +80,8 @@ int main()
 		case 3:cout << "Give me tg(alpha) :";
 			cin >> k;
 			cout << "Give me b :";
-			cin >> c; // c = b;
-			system("cls"); // clears the screen * Оч. скрин
+			cin >> c; // c = b;//
+			system("cls"); // clears the screen * Оч. скрин//
 			if (k == 0) { cout << "y=" << c; }
 			else if (k == 1) {
 				if (c >= 0) {
@@ -106,7 +106,7 @@ int main()
 			cin >> x0 >> y0;
 			cout << "Give me coordinates of second point x2,y2 : ";
 			cin >> x >> y;
-			system("cls"); // clears the screen * Оч. скрин
+			system("cls"); // clears the screen * Оч. скрин//
 			if ((x - x0) == 0 || (y - y0) == 0) {
 				cout << "Division by zero!!!";
 			}
@@ -125,13 +125,13 @@ int main()
 				else {
 					cout << "y=" << 1 / (x - x0)*(y - y0) << "x" << (x0 / (x - x0)*(y - y0) + y0);
 				}
-				//cout << "y=" << 1 / (x - x0)*(y - y0) << "x-" << (x0 / (x - x0)*(y - y0) + y0);
+				//cout << "y=" << 1 / (x - x0)*(y - y0) << "x-" << (x0 / (x - x0)*(y - y0) + y0);//
 			}
 			break;
 		case 5:cout << "Give me alpha:";
-			cin >> k;// k=alpha
+			cin >> k;// k=alpha//
 			cout << "Give me p - distance from (0;0) to line";
-			cin >> c;// c=p
+			cin >> c;// c=p//
 			if (cos(k / 180.0 / PI) / sin(k / 180.0 / PI) > 0) { cout << "y=" << c / sin(k / 180.0 / PI) << "-" << cos(k / 180.0 / PI) / sin(k / 180.0 / PI) << "x"; }
 			else if (cos(k / 180.0 / PI) / sin(k / 180.0 / PI) < 0) { cout << "y=" << c / sin(k / 180.0 / PI) << cos(k / 180.0 / PI) / sin(k / 180.0 / PI) << "x"; }
 			else if (sin(k / 180.0 / PI) == 0) { cout << "division by zero!!! Tracepaser"; }
@@ -141,7 +141,7 @@ int main()
 			cin >> x0 >> y0; //x0,y0=a,b
 			cout << "Give me point(dot) coordinates x1,y1:";
 			cin >> x >> y;// x=x1, y=y1
-			system("cls"); // clears the screen * Оч. скрин
+			system("cls"); // clears the screen * Оч. скрин//
 			if (x0 >= 0 && y >= 0) {
 				cout << "x=" << x << "+" << x0 << "*lambda(any number)" << endl;
 				cout << "y=" << y << "+" << y0 << "*lambda(any number)" << endl;
@@ -159,11 +159,11 @@ int main()
 				cout << "y=" << y << y0 << "*lambda(any number)" << endl;
 			}
 			break;
-		case 7:cout << "Give me coordinates of the first point x,y,z:"; //  <- must be handled
+		case 7:cout << "Give me coordinates of the first point x,y,z:"; //  <- must be handled//
 			cin >> x0 >> y0 >> z0;
 			cout << "Give me coordinates of the second point x,y,z:";
 			cin >> x >> y >> z;
-			system("cls"); // clears the screen * Оч. скрин
+			system("cls"); // clears the screen * Оч. скрин//
 			if ((x - x0) == 0 || (y - y0) == 0 || (z - z0) == 0) {
 				cout << "Division by zero!!!";
 			}
@@ -202,10 +202,10 @@ int main()
 			hangle = asin(sqrt((x*x0 + y*y0 + z*z0)*(x*x0 + y*y0 + z*z0)) / (sqrt(x*x + y*y + z*z)*sqrt(x0*x0 + y0*y0 + z0*z0))) * 180.0 / PI; // have some bugs :(
 			if (hangle == 90) {
 
-				cout << "Parallel"; // !
+				cout << "Parallel"; // ! //
 			}
 			else if (hangle == 0) {
-				cout << "Perpendicular"; // !
+				cout << "Perpendicular"; // ! //
 			}
 			else {
 				cout << "alpha = " <<90-hangle;
@@ -244,6 +244,6 @@ int main()
 		system("cls"); // clears the screen * Оч. скрин
 	}
 
-	// debug point
+	// debug point//
 	return 0;
 }
